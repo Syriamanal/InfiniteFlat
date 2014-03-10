38f2df18ccbd6e10f4a49529e59ea5544d06ec16
PEMapModder <pemapmodder1970@gmail.com>
2014-03-10 19:32:56 +0800

    Untitled commit

--- InfiniteFlat.php
+++ InfiniteFlat.php
@@ -14,7 +14,10 @@ class InfiniteFlat implements Plugin{
 	public static function request(){
 		return self::$inst;
 	}
-	public $cfg;
+	private static $runtime=false;
+	public static function get(){
+		return self::$runtime;
+	}
 	public function __construct(ServerAPI $a, $s=0){
 		$this->api=$api;
 		$this->server=ServerAPI::request();
@@ -23,6 +26,7 @@ class InfiniteFlat implements Plugin{
 	public function __destruct(){
 	}
 	public function init(){
+		InfiniteFlatSingleGenerator::init();
 		$this->dir=$this->api->plugin->configPath($this);
 		$noExt=$this->dir."settings.";
 		$ext="yml";
@@ -30,14 +34,123 @@ class InfiniteFlat implements Plugin{
 			$ext="txt";
 		if(defined("CONFIG_YAML"))eval("\$yml=CONFIG_YAML;");
 		else eval("\$yml=Config::YAML;");
-		$this->cfg=new Config($noExt.$ext, $yml, self::$defaultSettings);
+		$c=new Config($noExt.$ext, $yml, self::$defaultSettings);
+		self::$runtime=new InfiniteFlatRuntime(array($c->get("World generation efficiency"), $c->get("Overlapping chunks"), $c->get("Number of worlds preloaded for buffer for players near world margin"), $c->get("presets")));
+		
 	}
-	protected static $defaultSettings=array(
-"World generation efficiency"=>true,
-"Overlapping chunks not-squared"=>2,
-"Number of worlds preloaded for buffer for players near world margin"=>1,
-		);
+	protected final static $defaultSettings=array(
+			"World generation efficiency"=>true,
+			"Overlapping chunks"=>2,
+			"Number of worlds preloaded for buffer for players near world margin"=>1,
+			"default preset" => 0,
+	);
 }
+class InfiniteFlatRuntime extends Config{
+	public function __construct(Array $settings){
+		if(defined("CONFIG_YAML"))
+			eval("\$yml=CONFIG_YAML;");
+		else eval("\$yml=Config::YAML");
+		parent::__construct(ServerAPI::request()->api->plugin->configPath(InfiniteFlat::request())."runtime record - DO NOT EDIT", $yml);
+		$this->settings=$settings;
+	}
+}
+class InfiniteFlatLevel extends Config{
+	public function __construct(String $name, Array $settings){
+		if(defined("CONFIG_YAML"))
+			eval("\$yml=CONFIG_YAML;");
+		else eval("\$yml=Config::YAML");
+		parent::__construct(FILE_PATH."infinite worlds/$name.yml", $yml, array("gencfg"=>$settings));
+	}
+	public function init(){
+		$level=null;
+		$this->worlds[0][0]=$level;
+	}
+}
+class InfiniteFlatSingleGenerator implements LevelGenerator{
+	private $options;
+	public function __construct(array $options=array()){
+		$random=array_rand(self::getConfig()->get("presets"));
+		$this->options=isset($options["preset"])?$options["preset"]:$random;
+	}
+	public function init(Level $level, Random $random){
+		$this->l=$level;
+		$this->random=$random;
+	}
+	public function getSettings(){
+		return $this->options;
+	}
+	public function getSpawn(){
+		return new Vector3(128, 128, 128);
+	}
+	public function generateChunk($cx, $cz){
+		for($y=0; $y<8; $y++){
+			$this->chunks[$y]="";
+			
+			$this->chunks[$y].=$blocks.hex2bin($metas)."\x00\x00\x00\x00\x00\x00\x00\x00";
+		}
+		$this->l->setMiniChunk();
+	}
+	public function populateChunk($cx, $cz){
+	}
+	public static $config=false, $initialized=false;
+	public static function init(){
+		self::$config=new Config(ServerAPI::request()->api->plugin->configPath(request())."flat generaetor settings.yml", CONFIG_YAML, self::$defaultConfig);
+	}
+	public static $defaultConfig=array(
+			"help"=>array(
+					"presets"=>array("Follow the following example",
+							array("preset name"=>array(
+									"layers"=>array(
+											array("ID", "number of layers"),
+									),
+									"populators"=>array(
+											array("generation method (see below)", "extra data", "random count minimum", "random count maximum"),
+									),
+							)),
+							"generation methods listed below",
+							array(
+									"method name"=>array("explanation", array("extra data")),
+									"pillar"=>array("create random pillars on the topmost layer", array("Block ID", "height")),
+									"tree"=>array("grow a tree on the topmost layer", array("tree type, 0-3")),
+									"area"=>array("change part of the ground into this shape", array("Block ID", "random depth minimum", "random depth maximum", "shape - circle, square", "random side-length/radius minimum", "random size maximum")),
+							),
+					),
+			),
+			"presets"=>array(
+					"forest"=>array(
+							"layers"=>array(
+									array(BEDROCK, 1),
+									array(STONE, 59),
+									array(DIRT, 3),
+									array(GRASS, 1),
+							),
+							"populators"=>array(
+									array("tree", array(0), 150, 190),
+									array("tree", array(2), 80, 120),
+									array("area", array(STILL_WATER, 2, 4, "circle", 4, 7), 6, 10),
+							),
+					),
+					"desert"=>array(
+							"layers"=>array(
+									array(BEDROCK, 1),
+									array(STONE, 58),
+									array(SANDSTONE, 2),
+									array(SAND, 3),
+							),
+							"populators"=>array(
+									array("pillar", array(CACTUS, 2), 90, 130),
+									array("area", array(STILL_WATER, 2, 4, "circle", 4, 7), 6, 10),
+							),
+					),
+			)
+	);
+	public static function getConfig(){
+		if(!self::$initialized)
+			self::init();
+		return self::$config;
+	}
+}
+
 /*
 x x x x i i i i i i i i x x x x
 x x x x i i i i i i i i x x x x
